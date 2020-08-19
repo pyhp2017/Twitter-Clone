5b86b00055c881b8c939fc6f3f32a2bb02be3731
Ahmad <pyhp2017@gmail.com>
2020-08-19 20:26:45 +0430

    Working on Classes

---
+++ Profile.php
@@ -0,0 +1,16 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User Profile</title>
+</head>
+<body>
+
+<img src="Res/avatar.png" alt="Avatar">
+<h1>User: </h1>
+<h1>Email: </h1>
+<h1>Last Tweet: </h1>
+
+    
+</body>
+</html>
---
+++ User.php
@@ -0,0 +1,16 @@
+<?php
+
+/** */
+class User
+{
+    //Fields
+    private $userName;
+    private $password;
+    private $email;
+    private $lastTweetDate;
+    private $image;
+
+
+
+    
+}
